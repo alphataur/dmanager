f1e3b7462af795908658a05621c9dddea1e0d884
Nikhil Surya Mukhi <darksublimethoughts@gmail.com>
2021-07-29 02:28:16 +0530

    enhanced youtube-client media discovery and naming mechanisms

--- utils/clients/torrent-client.js
+++ utils/clients/torrent-client.js
@@ -86,7 +86,7 @@ module.exports = {
   TorrentClient
 }
 //let magnet = "cbffe1efd7de9beec9c5f2ba7d4a18e5738d6987"
-let magnet = process.argv[2] || "cbffe1efd7de9beec9c5f2ba7d4a18e5738d6987"
+let magnet = process.argv[2] || "0A9AC7CAFFE91E86105A83484D1B1269E0D1DD6B" //"cbffe1efd7de9beec9c5f2ba7d4a18e5738d6987"
 console.log(magnet)
 //let magnet = "magnet:?xt=urn:btih:EB2202C50FA8476E00BC8CDB891F26B9A170F9F0&dn=DMX+-+Exodus+%28Explicit%29+%282021%29+Mp3+320kbps+%5BPMEDIA%5D+%E2%AD%90%EF%B8%8F&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce"
 //
